58ca5d16981fb029b465fa39d699a62639136b39
Wilson <wilson@wizvision.com>
2016-06-06 11:21:09 +0800

    Missed out a file

---
+++ www/browser.js
@@ -0,0 +1,13 @@
+
+tabris.registerWidget("ESBrowser", {
+  _properties: {
+    "date": {type: "any", nocache: true}
+  },
+  _events: {
+    "change:date": {
+      trigger: function(event) {
+        this.trigger("change:date", this, event.date);
+      }
+    }
+  }
+});
